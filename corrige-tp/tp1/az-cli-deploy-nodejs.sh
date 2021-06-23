ad72dec4cc6ce8a884da5722f96fb181052d9d88
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-06-23 11:07:16 +0200

    Add solution for TP1

---
+++ corrige-tp/tp1/az-cli-deploy-nodejs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Se connecter si besoin 
+# en mode interactif (navigateur ouvert)
+# az login
+
+# uniquement en console
+# az login -u <username> -p <password>
+
+# Création groupe ressource
+az group create \
+    -l eastus \
+    -n tp1-vanessa
+
+# Création VM Azure
+az vm create \
+  --resource-group tp1-vanessa \
+  --name node-vm \
+  --location eastus \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --generate-ssh-keys \
+  --custom-data cloud-init-github.txt
+
+# Ouverture du port 80 sur la VM
+
+az vm open-port \
+  --port 80 \
+  --resource-group tp1-vanessa \
+  --name node-vm
